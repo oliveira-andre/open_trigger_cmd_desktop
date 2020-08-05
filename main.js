841df23de3ae7e93202d8a692a46c843cbc82aca
Andre Oliveira <desmonplays@gmail.com>
2020-08-05 13:09:31 -0400

    feat: default view of electron app

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.whenReady().then(createWindow)
