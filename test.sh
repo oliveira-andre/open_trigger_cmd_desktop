e0d8ff8816985bcef1af970f12154089b520be7a
Andre Oliveira <desmonplays@gmail.com>
2020-08-08 16:38:04 -0300

    feat: removing test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-open "https://newpropay.portaldorh.com.br/portal_lcwb/Produtos/SAAA/Principal2.aspx?amb_selecionado=128&abrir_nova_janela=N&eh_mdesigner=N&nome_portal=447564524B316E7A56636764317364716D53577639513D3D"
-
-open "http://locaweb.rocket.chat/"
-
-open /System/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal
-
